0f560e7395ef4312cb4ec99a1ed56a31131c9bd9
gxranas <g.x.ranas@gmail.com>
2023-06-17 15:04:47 +0800

    Add Cors

--- src/routes/UsersRoute.js
+++ src/routes/UsersRoute.js
@@ -8,6 +8,12 @@ const router = express.Router();
 
 
 router.post("/register", async (req,res) =>{
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+    
     const {username, password} = req.body;
     const user = await UserModel.findOne({username});
 
